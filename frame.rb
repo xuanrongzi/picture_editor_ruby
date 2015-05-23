cf27596661fcfddae4b4ffa1399ebcd904ac2d17
xuanrongzi <leq94@live.unc.edu>
2015-05-22 23:32:08 -0400

    Add function and testing
    iterating pattern in ruby is interesting

--- frame.rb
+++ frame.rb
@@ -1,5 +1,6 @@
 require_relative 'pixel'
 class Frame
+  include Enumerable
   def initialize(*args)
     @height,@width=args[0],args[1]
     case args.length
@@ -8,25 +9,95 @@ class Frame
       when 3
         @ini_color=args[2]
       else
-        raise Invaild, 'Frame only receive 2 or 3 parameters'
+        raise Error, 'Frame only receive 2 or 3 parameters'
     end
+    @title=''
     make_frame
   end
 
   def make_frame
     row,col=@height,@width
     @frame=Array.new(row)
-    @frame.map!{Array.new(col,@ini_color.clone)}
+    @frame.map! { Array.new(col) }
 
+    0.upto(@height-1) do |i|
+      0.upto(@width-1) do |j|
+        @frame[i][j]=Pixel.new(*@ini_color.rgb)
+      end
+    end
+  end
+
+  def title
+    @title
+  end
+  def title=(t)
+    @title=t if (t!=nil)|(t.is_a?String)
+  end
+
+  # @param [Frame] new_frame
+  def equal?(new_frame)
+    if(new_frame.is_a?Frame)
+      if(new_frame.width==@width&&new_frame.length==@length)
+
+        0.upto(@height-1)do |i|
+          0.upto(@width-1)do |j|
+            return nil if(!(@frame[i][j].rgb==new_frame[i][j].rgb))
+          end
+        end
+      end
+        return true
+    end
+     nil
+  end
+
+  def to_s
+    "Frame: #{@title}(Height:#{@height},Width:#{@width})"
+  end
+
+  def each
+
+    0.upto(@height-1)do |i|
+      0.upto(@width-1)do |j|
+        yield @frame[i][j]
+      end
+    end
+
+  end
+
+  def separate
+    #divided the frame into r frame, g frame and b frame
+    frame_r=Array.new(@height){Array.new(@width)}
+    frame_g=Array.new(@height){Array.new(@width)}
+    frame_b=Array.new(@height){Array.new(@width)}
+    0.upto(@height-1) do |i|
+      0.upto(@width-1) do |j|
+          a=@frame[i][j].rgb
+          frame_r[i][j]=Pixel.new(a[0],0,0)
+          frame_g[i][j]=Pixel.new(0,a[1],0)
+          frame_b[i][j]=Pixel.new(0,0,a[2])
+      end
+    end
+  [frame_r,frame_g,frame_b]
   end
 
   def get(row,col)
     @frame[row][col]
   end
 
-  def put(row,col,pixel)
-    @frame[row][col]=pixel if pixel.instance_of? Pixel
+  def [](row, col)
+    if (row<0)|(col<0)|(row>=@height)|(col>=@width)
+      return nil
+    end
+    @frame[row][col]
   end
+
+  def []=(row,col,pixel)
+    if   !(pixel.instance_of?Pixel)
+          return nil
+    end
+    @frame[row][col]=pixel if ((row>=0)|(col>=0)|(row<@height)|(col<@width))
+  end
+  alias put []=
 
   def width
     @width
@@ -39,8 +110,15 @@ class Frame
 
 
 
-  a=Frame.new(2,2)
-  a.put(1,1,Pixel.new(5,3,4))
-  a.put(1,1,1)
+  a=Frame.new(3,3,Pixel.new(1,2,3))
+  a[1,1]=Pixel.new(1,1,1)
+  a[1,0]=Pixel.new(2,2,2)
+  a[0,1].r=11
+  a[0,0].r=10
+  a[2,2]=a[1,1]
   print  a.get(1,1)
+  print "--\n"
+
+  Pixel.new(25,1,1)
+  a.each{|x| print x}
 end
--- pixel.rb
+++ pixel.rb
@@ -5,31 +5,29 @@ class Pixel
 
       @red, @green, @blue=red, green, blue
     else
-      raise Invalid, "Wrong type for Pixel"
+      raise Error, 'Wrong type for Pixel'
     end
     if (red>255)|(red<0)|(green>255)|(green<0)|(blue>255)|(blue<0)
-      raise Invalid,  "Color Out of Bound, #{red},#{green}, #{blue}"
+      raise Error,  "Color Out of Bound, #{red},#{green}, #{blue}"
     end
-    @brightness=0.2126 * red + 0.7152 * green + 0.0722 * blue
+    brtn
   end
 
-
-  def rgb()
-    return[@red,@green,@blue]
+  def brtn
+    @brightness=0.2126 * @red + 0.7152 * @green + 0.0722 * @blue
+  end
+  def rgb
+    [@red,@green,@blue]
   end
 
   def equal?(new_pixel)
-    if new_pixel.rgb=[@red,@green,@blue]
-      return true
-    else
-      return false
-    end
+    new_pixel.rgb=[@red, @green, @blue] ?  true :  false
 
   end
   alias eql? equal?
 
   def to_s
-    return  "Pixel with red=#{@red}, green=#{@green}, blue=#{@blue} and brightness=#{ @brightness.round(3)}"+"\n"
+    "Pixel with red=#{@red}, green=#{@green}, blue=#{@blue} and brightness=#{ @brightness.round(3)}"+"\n"
   end
   alias old_to_s to_s
   def inspect
@@ -52,4 +50,26 @@ class Pixel
   alias b blue
   alias br brightness
 
+  def red=(r)
+    @red=r
+    brtn
+  end
+  def green=(g)
+    @green=g
+    brtn
+  end
+  def blue=(b)
+    @blue=b
+    brtn
+  end
+  def rgb=(*args)
+    @red, @green, @blue=args[0],args[1],args[2] if args.length==3
+    if args.length==1 &&(args[0].is_a?Array)
+      @red,@green,@blue=args[0][0],args[0][1],args[0][2]
+    end
+    brtn
+  end
+  alias  r= red=
+  alias g= green=
+  alias b= blue=
 end
